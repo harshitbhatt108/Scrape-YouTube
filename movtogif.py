a94314060f1577d9b6b5ee9dc883d3f83ce5ed16
harshitbhatt108 <harshitbhatt53@gmail.com>
2020-08-04 11:34:01 +0530

    everything Working

---
+++ movtogif.py
@@ -0,0 +1,23 @@
+import imageio
+import os
+
+clip = os.path.abspath('mov.mp4');
+
+def gifMaker(inputPath, targetFormat):
+	outputPath = os.path.splitext(inputPath)[0] + targetFormat
+	print(f'Converting {inputPath} \n to {outputPath}')
+
+	reader = imageio.get_reader(inputPath)
+	fps = reader.get_meta_data()['fps']
+
+	writer = imageio.get_writer(outputPath, fps=fps)
+
+
+	for frames in reader:
+		writer.append_data(frames)
+		print(f'frames {frames}')
+	print("done")
+	writer.close()
+
+
+gifMaker(clip, '.gif')
